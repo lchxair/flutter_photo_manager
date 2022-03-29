7f4bf1c3c51e902d59d598c3cf6132565acb8444
lchx <lchxair@163.com>
2022-03-29 15:07:56 +0800

    1、压缩JPG图片时，增加指定体积最大为10M；

--- ios/Classes/core/PMImageUtil.m
+++ ios/Classes/core/PMImageUtil.m
@@ -31,7 +31,8 @@
     if (type == PMThumbFormatTypePNG) {
         resultData = UIImagePNGRepresentation(image);
     } else {
-        resultData = UIImageJPEGRepresentation(image, quality);
+        //        resultData = UIImageJPEGRepresentation(image, quality);
+        resultData = [PMImageUtil compressOriginalImage:image toMaxDataSizeKBytes:10 * 1000];
     }
     
     return resultData;
@@ -39,5 +40,30 @@
 #endif
 }
 
+/**
+ * 压缩JPG图片到指定文件大小
+ *
+ * @param image 目标图片
+ * @param size 目标大小（最大值）
+ *
+ * @return 返回的图片文件
+ */
++ (NSData *)compressOriginalImage:(UIImage *)image toMaxDataSizeKBytes:(CGFloat)size{
+  NSData *data = UIImageJPEGRepresentation(image, 1.0);
+  CGFloat dataKBytes = data.length/1000.0;
+  CGFloat maxQuality = 0.9f;
+  CGFloat lastData = dataKBytes;
+  while (dataKBytes > size && maxQuality > 0.01f) {
+    maxQuality = maxQuality - 0.01f;
+    data = UIImageJPEGRepresentation(image, maxQuality);
+    dataKBytes = data.length / 1000.0;
+    if (lastData == dataKBytes) {
+      break;
+    }else{
+      lastData = dataKBytes;
+    }
+  }
+  return data;
+}
 
 @end
